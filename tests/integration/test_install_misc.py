edd5227ef801501e29fd8a47cdb94a896ab6c90c
Daniel <daniel@developerdan.com>
2023-09-19 23:45:26 -0400

    Add mitmproxy as a way to test self-signed certificates and ssl_verify

--- tests/integration/test_install_misc.py
+++ tests/integration/test_install_misc.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 
-from .conftest import DEFAULT_PRIVATE_PYPI_SERVER
+from pipenv.utils.shell import temp_environ
+from .conftest import DEFAULT_PRIVATE_PYPI_SERVER, pipenv_instance_private_pypi
 
 
 @pytest.mark.urls
@@ -25,3 +28,82 @@ plette = {{file = "{file_uri}", extras = ["validation"]}}
         assert c.returncode == 0
         assert "plette" in p.lockfile["default"]
         assert "cerberus" in p.lockfile["default"]
+
+
+@pytest.mark.ssl
+@pytest.mark.needs_mitmproxy
+@pytest.mark.extras
+@pytest.mark.install
+def test_install_verify_custom_ssl_verify(pipenv_instance_private_pypi_secure, cert_path):
+    """Ensure environment variables are not expanded in lock file.
+        """
+    with temp_environ(), pipenv_instance_private_pypi_secure() as p:
+        os.environ["REQUESTS_CA_BUNDLE"] = cert_path
+
+        with open(p.pipfile_path, "w") as f:
+            f.write(
+                f"""
+[[source]]
+url = "{p.index_url}"
+verify_ssl = true
+name = 'mockpi'
+
+[packages]
+six = {{}}
+"""
+            )
+
+        c = p.pipenv("install -v")
+        assert c.returncode == 0
+        assert "six" in p.lockfile["default"]
+
+
+@pytest.mark.ssl
+@pytest.mark.needs_mitmproxy
+@pytest.mark.extras
+@pytest.mark.install
+def test_install_verify_custom_ssl_no_verify(pipenv_instance_private_pypi_secure, cert_path):
+    """Ensure environment variables are not expanded in lock file.
+        """
+    with pipenv_instance_private_pypi_secure() as p:
+        with open(p.pipfile_path, "w") as f:
+            f.write(
+                f"""
+[[source]]
+url = "{p.index_url}"
+verify_ssl = false
+name = 'mockpi'
+
+[packages]
+six = {{}}
+"""
+            )
+
+        c = p.pipenv("install -v")
+        assert c.returncode == 0
+        assert "six" in p.lockfile["default"]
+
+
+@pytest.mark.ssl
+@pytest.mark.needs_mitmproxy
+@pytest.mark.extras
+@pytest.mark.install
+def test_install_verify_custom_ssl_verify_fail(pipenv_instance_private_pypi_secure, cert_path):
+    """Ensure environment variables are not expanded in lock file.
+        """
+    with pipenv_instance_private_pypi_secure() as p:
+        with open(p.pipfile_path, "w") as f:
+            f.write(
+                f"""
+[[source]]
+url = "{p.index_url}"
+verify_ssl = true
+name = 'mockpi'
+
+[packages]
+six = {{}}
+"""
+            )
+
+        c = p.pipenv("install -v")
+        assert c.returncode == 1
